c0995b5396134c9bb417c45540f39db7d3ae1148
Adriano Ferreira <a.r.ferreira@gmail.com>
2018-04-01 13:58:30 -0700

    "Docs" for WebService::Pinterest::X

--- lib/WebService/Pinterest/X.pm
+++ lib/WebService/Pinterest/X.pm
@@ -11,10 +11,6 @@ sub throw {
     die shift->new(shift);
 }
 
-# key
-# value
-# message
-
 sub as_string {
     my $self = shift;
     return sprintf $self->{message}, $self->{key}, $self->{value};
@@ -22,3 +18,42 @@ sub as_string {
 
 1;
 
+=encoding utf8
+
+=head1 NAME
+
+WebService::Pinterest::X - Exceptions for WebService::Pinterest
+
+=head1 SYNOPSIS
+
+    use WebService::Pinterest::X;
+
+    $x = WebService::Pinterest::X->new({ message => '%s %s', key => 'k', value => 'foo' } );
+
+=head1 METHODS
+
+=head2 as_string
+
+    $str = $x->as_string;
+
+=head2 new
+
+    $x = WebService::Pinterest::X->new({ message => '%s %s', key => 'k', value => 'foo' } );
+
+=over 4
+
+=item key
+
+=item value
+
+=item message
+
+=back
+
+Constructor.
+
+=head2 throw
+
+    WebService::Pinterest::X->throw({ message => '%s %s', key => 'k', value => 'foo' } );
+
+=cut
